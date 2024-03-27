aade1b196df2afb8fb5b9a86ce1f39a741323bef
adrianhhd <118419967+adrianhhd@users.noreply.github.com>
2024-03-27 13:38:08 +0700

    Update: Time Format GMT +7 (3)

--- repository/visits_repository.go
+++ repository/visits_repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"fmt"
 	"portfolio-api/entity"
 	"time"
 )
@@ -16,11 +15,17 @@ type visitsRepository struct {
 }
 
 func (v *visitsRepository) Insert(visitor entity.Visitor) (entity.Visitor, error) {
-	fmt.Println(v.db)
-	err := v.db.QueryRow(
+	indonesia, err := time.LoadLocation("Europe/Moscow")
+    if err != nil {
+        panic(err)
+    }
+	
+	now := time.Now().In(indonesia)
+
+	err = v.db.QueryRow(
 		"INSERT INTO tx_visits (Visitor_IP, Date_Created) VALUES (?, ?) RETURNING Visit_ID, Visitor_IP, Date_Created",
 		visitor.Visitor_IP,
-		time.Now().Format("2006-01-02 15:04:05 GMT+07"),
+		now,
 	).Scan(&visitor.Visit_ID, &visitor.Visitor_IP, &visitor.Date_Created)
 
 	if err != nil {
